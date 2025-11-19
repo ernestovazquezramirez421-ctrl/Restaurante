c049644b9e2223bf18056c3e4f9ae219f1a9d792
Ernesto Vázquez <ernestovazquezramirez421-ctrl>
2025-11-18 21:29:55 -0600

    Intento

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-echo "DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE"
-
+# Convert static asset files
 python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
 python manage.py migrate
--- manage.py
+++ manage.py
@@ -3,12 +3,6 @@
 import os
 import sys
 
-# ---------------------------
-# Solución para Render
-# ---------------------------
-# Asegura que la carpeta raíz del proyecto esté en sys.path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# ---------------------------
 
 def main():
     """Run administrative tasks."""
