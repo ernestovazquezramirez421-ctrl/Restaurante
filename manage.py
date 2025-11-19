5bc0d2c56515b98eb36fb8917ee856db508a2c89
Ernesto Vázquez <ernestovazquezramirez421-ctrl>
2025-11-18 21:21:11 -0600

    Correcion manage

--- manage.py
+++ manage.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+# ---------------------------
+# Solución para Render
+# ---------------------------
+# Asegura que la carpeta raíz del proyecto esté en sys.path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# ---------------------------
 
 def main():
     """Run administrative tasks."""
