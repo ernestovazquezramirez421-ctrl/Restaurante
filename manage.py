ff95ef0e30c4c0607d4a0fecb0ec141de456bc2d
Ernesto Vázquez <ernestovazquezramirez421-ctrl>
2025-11-18 22:17:45 -0600

    Cambios

--- manage.py
+++ manage.py
@@ -3,23 +3,20 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    # Forzar variable de entorno para Render gratuito
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restaurante_marisco.settings")
-    
-    # Forzar la ruta del proyecto
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    if current_path not in sys.path:
-        sys.path.append(current_path)
-
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurante_marisco.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Make sure it's installed and available on your PYTHONPATH."
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Forzar variable de entorno por seguridad
-export DJANGO_SETTINGS_MODULE=restaurante.settings
-
-# Ejecutar Gunicorn
-gunicorn restaurante_marisco.wsgi:application --bind 0.0.0.0:$PORT --workers 4
