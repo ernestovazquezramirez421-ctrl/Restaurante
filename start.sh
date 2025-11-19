b40efb6b3726bf57e26af070deb92c31318466bb
Ernesto Vázquez <ernestovazquezramirez421-ctrl>
2025-11-18 21:32:21 -0600

    Se agrega el archivo start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Forzar variable de entorno por seguridad
+export DJANGO_SETTINGS_MODULE=restaurante.settings
+
+# Ejecutar Gunicorn
+gunicorn restaurante.wsgi:application --bind 0.0.0.0:$PORT --workers 4
